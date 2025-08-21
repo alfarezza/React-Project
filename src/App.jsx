288c2d76fe57278502f1792109bed4eb4d884f03
alfarezza <alfarezza08@gmail.com>
2025-08-21 08:26:04 +0700

    updated logo

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
           <img src={scrimbaLogo} className="logo react" alt="Scrimba logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Scrimba</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
